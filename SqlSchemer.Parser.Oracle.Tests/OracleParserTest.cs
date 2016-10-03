aafaf98d5741514a54a5df29ed624b6e30115ff3
Soloydenko-C <Igor.Soloydenko@seattle.gov>
2016-10-03 15:38:56 -0700

    Add SqlSchemer.Parser.Oracle.Tests

---
+++ SqlSchemer.Parser.Oracle.Tests/OracleParserTest.cs
@@ -0,0 +1,20 @@
+﻿using SqlSchemer.Model.Interfaces;
+using Xunit;
+
+namespace SqlSchemer.Parser.Oracle.Tests
+{
+    public class OracleParserTest
+    {
+        private readonly OracleParser sut = new OracleParser();
+
+        [Fact]
+        public void T1()
+        {
+            string[] sqlSchemaScriptLines = null;
+
+            ISqlSchema result = sut.ParseScript(sqlSchemaScriptLines);
+
+            Assert.NotNull(result);
+        }
+    }
+}
